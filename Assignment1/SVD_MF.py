dac6cfb66e0ead4b2edccc2a55b27d167c349b72
tamir.zec <tamir.zec@gmail.com>
2021-03-13 14:08:44 +0200

    blah

---
+++ Assignment1/SVD_MF.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+class RecommenderSystem:
+    def __init__(self, ratings, learning_rate=0.05, sgd_step=0.05, latent_factors=20, item_bias=0.0, user_bias=0.0):
+        self.learning_rate = learning_rate
+        self.sgd_step = sgd_step
+        self.latent_factors = latent_factors
+        self.item_bias = item_bias
+        self.user_bias = user_bias
+
+
+    def Load(self,fileName):
+        
+        load_directory = os.path.join()
+
