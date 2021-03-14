4432b60bc35ee22955fe50f10599e15584882da2
sharonhi <sharonhi@campus.technion.ac.il>
2021-03-14 16:12:19 +0200

    basic model

--- Assignment1/SVD_MF.py
+++ Assignment1/SVD_MF.py
@@ -6,92 +6,112 @@ from scipy import sparse
 
 
 class RecommenderSystem:
-    def __init__(self, data_path, learning_rate=0.05, sgd_step_size=0.05, latent_factors=20, sgd_batch_size=100):
+
+    def __init__(self, data_path, learning_rate=0.05, sgd_step_size=0.05, latent_factors=20):
+
         self.learning_rate = learning_rate
         self.sgd_step_size = sgd_step_size
         self.latent_factors = latent_factors
         self.data_path = data_path
-        self.sgd_batch_size = sgd_batch_size
 
     def Load(self):
         load_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), self.data_path)
 
-        ratings = []
-        users = []
-        business = []
-
+        ratings, users, items = [], [], []
         for df in pd.read_csv(load_directory, chunksize=10000, usecols=['user_id', 'business_id', 'stars']):
             ratings.extend(df['stars'].values)
             users.extend(df['user_id'].values)
-            business.extend(df['business_id'].values)
+            items.extend(df['business_id'].values)
 
         users = np.array(users)
-        business = np.array(business)
+        items = np.array(items)
         self.total_users = len(np.unique(users))
-        self.total_business = len(np.unique(business))
+        self.total_items = len(np.unique(items))
+        # Initialize bias vectors
         self.user_bias = np.random.random(self.total_users)
-        self.item_bias = np.random.random(self.total_business)
+        self.item_bias = np.random.random(self.total_items)
+
         self.total_ratings = len(ratings)
         self.avg_ratings = np.mean(ratings)
         self.users2id = {user: idx for idx, user in enumerate(np.unique(users))}
-        self.business2id = {business: idx for idx, business in enumerate(np.unique(business))}
+        self.item2id = {item: idx for idx, item in enumerate(np.unique(items))}
         map_users = lambda x: self.users2id[x]
-        map_business = lambda x: self.business2id[x]
+        map_item = lambda x: self.item2id[x]
 
         users = np.array([map_users(user_i) for user_i in users])
-        business = np.array([map_business(business_i) for business_i in business])
+        items = np.array([map_item(item_i) for item_i in items])
 
-        self.ratings_matrix = sparse.csr_matrix((ratings, (np.array(users), np.array(business))),
-                                                shape=(self.total_users, self.total_business))
+        self.ratings_matrix = sparse.csr_matrix((ratings, (np.array(users), np.array(items))),
+                                                shape=(self.total_users, self.total_items))
 
-    def rmse(self, ratings, predictions):
-        return np.square(sum(np.power(ratings - predictions, 2)) / self.total_ratings)
+        self.initialize_data()
 
-    def TrainBaseModel(self):
-        self.initialize_matrices()
+    def calc_rmse(self, ratings, predictions):
+        N = len(predictions.nonzero()[0])
+        result = (ratings - predictions).power(2)
+        return np.square(result.sum() / N)
+
+    def TrainBaseModel(self, n_iter=10):
+        rmse = []
+        # shuffle entries and calculate SGD for each user/item
+        sgd_indices = np.arange(len(self.idx_row))
+        for _ in range(n_iter):
+            np.random.shuffle(sgd_indices)
+            self.sgd_step(sgd_indices)
+            predictons = self.calc_predictions()
+            rmse.append(self.calc_rmse(self.val_rating_matrix, predictons))
+
+        # Save parameters and rmse result to file
+        print(rmse)
+
+    def initialize_data(self):
+        self.users_matrix = np.random.rand(self.total_users, self.latent_factors)
+        self.items_matrix = np.random.rand(self.latent_factors, self.total_items)
+        self.idx_row, self.idx_col, self.rating_val = sparse.find(self.ratings_matrix)
         # split to train/validation
-        train_ratings_matrix, val_rating_matrix = self.train_validation_split()
-        # train iterations that do all
-        return 0
-
-    def initialize_matrices(self):
-        self.users_matrix = np.random.rand((self.total_users, self.latent_factors))
-        self.item_matrix = np.random.rand((self.total_business, self.latent_factors))
-
-    def train_iterations(self, n_iter=10):
-        # sample n entries - add a hyper parameter for batch size
-        self.idx_row, self.idx_col = self.ratings_matrix.nonzero()
-        # choose sample of batch size to calcuate sgd
-
-        # calculate SGD for each user/item
-        # move values towards the derivitive
-        # self.ratings_matrix[] - self.users_matrix*self.item_matrix
-        return 0
+        self.train_ratings_matrix, self.val_rating_matrix = self.train_validation_split()
 
     def train_validation_split(self, validation_size=0.2):
-        np.random.choice(range(self.idx_row), size=np.round(len(self.idx_row) * validation_size))
-        return 0, 0
+        validation_indices = np.random.choice(range(len(self.idx_row)), size=int(len(self.idx_row) * validation_size))
+        train_ratings_matrix = self.ratings_matrix.copy()
+        # Place zeroes in the validation set entries and remove them from the matrix
+        for u_id, i_id in zip(self.idx_row[validation_indices], self.idx_col[validation_indices]):
+            train_ratings_matrix[u_id, i_id] = 0
+        train_ratings_matrix.eliminate_zeros()
+        # Create new matrix for the validation set
+        val_rating_matrix = sparse.csr_matrix((self.rating_val[validation_indices],
+                                               (self.idx_row[validation_indices], self.idx_col[validation_indices])),
+                                              shape=(self.total_users, self.total_items))
 
-    def sgd_step(self, validation_idxs):
-        for idx in validation_idxs:
-            u = self.users_matrix[idx]
-            i = self.item_matrix[idx]
+        return train_ratings_matrix, val_rating_matrix
+
+    def sgd_step(self, sgd_indices):
+        for idx in sgd_indices:
+            u = self.idx_row[idx]
+            i = self.idx_col[idx]
             prediction = self.calc_rating(u, i)
-            e = (self.ratings_matrix[u, i] - prediction)  # error
+            # Error
+            e = (self.ratings_matrix[u, i] - prediction)
             # Update biases
             self.user_bias[u] += self.sgd_step_size * (e - self.learning_rate * self.user_bias[u])
             self.item_bias[i] += self.sgd_step_size * (e - self.learning_rate * self.item_bias[i])
 
             # Update latent factors
-            self.item_matrix[i, :] += self.sgd_step_size * (e * self.users_matrix[u, :] - self.learning_rate * self.i)
-            self.users_matrix[u, :] += self.sgd_step_size * (e * self.item_matrix[i, :] - self.learning_rate *
-                                                             self.users_matrix[u, :])
+            self.items_matrix[:, i] += self.sgd_step_size * (
+                    e * self.users_matrix[u, :] - self.learning_rate * self.items_matrix[:, i])
+            self.users_matrix[u, :] += self.sgd_step_size * (
+                    e * self.items_matrix[:, i] - self.learning_rate * self.users_matrix[u, :])
 
     def calc_rating(self, user, item):
-        return self.item_bias[item] + self.user_bias[user] + (user * item)
+        return self.avg_ratings + self.item_bias[item] + self.user_bias[user] + (
+            np.dot(self.users_matrix[user, :], self.items_matrix[:, item]))
 
+    def calc_predictions(self):
+        row_idx, col_idx = self.val_rating_matrix.nonzero()
+        data = []
+        for u_id, i_id in zip(row_idx, col_idx):
+            prediction = self.calc_rating(u_id, i_id)
+            data.append(prediction)
 
-
-if __name__ == '__main__':
-    recsys = RecommenderSystem('data/userTrainData.csv')
-    recsys.Load()
+        predictions = sparse.csr_matrix((data, (row_idx, col_idx)), shape=(self.total_users, self.total_items))
+        return predictions
--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,5 +1,11 @@
 import numpy as np
+
+from SVD_MF import RecommenderSystem
+
 RANDOM_SEED = 4
 
-
-np.random.seed(RANDOM_SEED)
+if __name__ == '__main__':
+    np.random.seed(RANDOM_SEED)
+    recsys = RecommenderSystem('data/userTrainData.csv')
+    recsys.Load()
+    recsys.TrainBaseModel()
