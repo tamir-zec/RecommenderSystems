c7a06d920849bcc8f6f55841cc2e05a057012078
tamir.zec <tamir.zec@gmail.com>
2021-03-14 13:26:34 +0200

    -added biases as vectors -added sgd implementation -need to implement train_validation

--- Assignment1/SVD_MF.py
+++ Assignment1/SVD_MF.py
@@ -6,13 +6,10 @@ from scipy import sparse
 
 
 class RecommenderSystem:
-    def __init__(self, data_path, learning_rate=0.05, sgd_step=0.05, latent_factors=20, item_bias=0.0, user_bias=0.0,
-                 sgd_batch_size=100):
+    def __init__(self, data_path, learning_rate=0.05, sgd_step_size=0.05, latent_factors=20, sgd_batch_size=100):
         self.learning_rate = learning_rate
-        self.sgd_step = sgd_step
+        self.sgd_step_size = sgd_step_size
         self.latent_factors = latent_factors
-        self.item_bias = item_bias
-        self.user_bias = user_bias
         self.data_path = data_path
         self.sgd_batch_size = sgd_batch_size
 
@@ -32,6 +29,8 @@ class RecommenderSystem:
         business = np.array(business)
         self.total_users = len(np.unique(users))
         self.total_business = len(np.unique(business))
+        self.user_bias = np.random.random(self.total_users)
+        self.item_bias = np.random.random(self.total_business)
         self.total_ratings = len(ratings)
         self.avg_ratings = np.mean(ratings)
         self.users2id = {user: idx for idx, user in enumerate(np.unique(users))}
@@ -45,35 +44,52 @@ class RecommenderSystem:
         self.ratings_matrix = sparse.csr_matrix((ratings, (np.array(users), np.array(business))),
                                                 shape=(self.total_users, self.total_business))
 
-        def rmse(self, ratings, predictions):
-            return np.square(sum(np.power(ratings - predictions, 2)) / self.total_ratings)
+    def rmse(self, ratings, predictions):
+        return np.square(sum(np.power(ratings - predictions, 2)) / self.total_ratings)
 
-        def TrainBaseModel(self):
-            self.initialize_matrices()
+    def TrainBaseModel(self):
+        self.initialize_matrices()
+        # split to train/validation
+        train_ratings_matrix, val_rating_matrix = self.train_validation_split()
+        # train iterations that do all
+        return 0
 
-            # split to train/validation
+    def initialize_matrices(self):
+        self.users_matrix = np.random.rand((self.total_users, self.latent_factors))
+        self.item_matrix = np.random.rand((self.total_business, self.latent_factors))
 
-            # train iterations that do all
+    def train_iterations(self, n_iter=10):
+        # sample n entries - add a hyper parameter for batch size
+        self.idx_row, self.idx_col = self.ratings_matrix.nonzero()
+        # choose sample of batch size to calcuate sgd
 
-            return 0
+        # calculate SGD for each user/item
+        # move values towards the derivitive
+        # self.ratings_matrix[] - self.users_matrix*self.item_matrix
+        return 0
 
-        def initialize_matrices():
-            self.users_matrix = np.random.rand((self.total_users, self.latent_factors))
-            self.item_matrix = np.random.rand((self.total_business, self.latent_factors))
+    def train_validation_split(self, validation_size=0.2):
+        np.random.choice(range(self.idx_row), size=np.round(len(self.idx_row) * validation_size))
+        return 0, 0
 
-        def train_iterations(n_iter=10):
-            # sample n entries - add a hyper parameter for batch size
-            self.idx_row, self.idx_col = self.ratings_matrix.nonzero()
-            # choose sample of batch size to calcuate sgd
+    def sgd_step(self, validation_idxs):
+        for idx in validation_idxs:
+            u = self.users_matrix[idx]
+            i = self.item_matrix[idx]
+            prediction = self.calc_rating(u, i)
+            e = (self.ratings_matrix[u, i] - prediction)  # error
+            # Update biases
+            self.user_bias[u] += self.sgd_step_size * (e - self.learning_rate * self.user_bias[u])
+            self.item_bias[i] += self.sgd_step_size * (e - self.learning_rate * self.item_bias[i])
 
-            # calculate SGD for each user/item
-            # move values towards the derivitive
-            # self.ratings_matrix[] - self.users_matrix*self.item_matrix
-            return 0
+            # Update latent factors
+            self.item_matrix[i, :] += self.sgd_step_size * (e * self.users_matrix[u, :] - self.learning_rate * self.i)
+            self.users_matrix[u, :] += self.sgd_step_size * (e * self.item_matrix[i, :] - self.learning_rate *
+                                                             self.users_matrix[u, :])
 
-        def sgd_step(self):
-            #todo: implement!!!!!!
-            pass
+    def calc_rating(self, user, item):
+        return self.item_bias[item] + self.user_bias[user] + (user * item)
+
 
 
 if __name__ == '__main__':
