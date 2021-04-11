7879e558321e3f819e724389f7b01fd5193eb4e8
sharonhi <sharonhi@campus.technion.ac.il>
2021-04-11 13:32:16 +0300

    run hybrid

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -24,9 +24,6 @@ def TrainHybridModel():
     recsys1.initialize_data()
     recsys1.TrainBaseModel(n_iter=7)
     base_model_predictions, _ = recsys1.PredictRating()
-    # toDO: remove
-    print("RMSE for Base" + str(recsys1.calc_rmse(recsys1.test_ratings_matrix, base_model_predictions)))
-    print("MAE for Base" + str(recsys1.calc_mae(recsys1.test_ratings_matrix, base_model_predictions)))
 
     # Initialize advanced model
     recsys2 = RecommenderSystem('data', learning_rate=0.03, sgd_step_size=0.03, implicit_lrate=0.05,
@@ -36,23 +33,15 @@ def TrainHybridModel():
     recsys2.initialize_data()
     recsys2.TrainAdvancedModel(n_iter=7)
     advanced_model_predictions, _ = recsys2.PredictRating()
-    # toDO: remove
-    print("RMSE for SVD++" + str(recsys1.calc_rmse(recsys1.test_ratings_matrix, advanced_model_predictions)))
-    print("MAE for SVD++" + str(recsys1.calc_mae(recsys1.test_ratings_matrix, advanced_model_predictions)))
 
     # Initialize content model
     recsys3 = RecommenderSystem('data', advanced=False, content=True, train_mode=TRAIN_MODE)
     recsys3.Load()
     recsys3.TrainContentModel()
     content_model_predictions, _ = recsys3.PredictRating()
-    # toDO: remove
-    print("RMSE for content regression" + str(recsys1.calc_rmse(recsys1.test_ratings_matrix, content_model_predictions)))
-    print("MAE for content regression" + str(recsys1.calc_mae(recsys1.test_ratings_matrix, content_model_predictions)))
 
     for weights in [[1 / 3, 1 / 3, 1 / 3], [0.5, 0.3, 0.2], [0.5, 0.4, 0.1], [0.4, 0.4, 0.2]]:
-        hybrid_predictions = (weights[0] * base_model_predictions +
-                              weights[1] * advanced_model_predictions +
-                              weights[2] * content_model_predictions) / 3
+        hybrid_predictions = weights[0] * base_model_predictions + weights[1] * advanced_model_predictions + weights[2] * content_model_predictions
         if TRAIN_MODE:
             rmse = recsys1.calc_rmse(recsys1.val_rating_matrix, hybrid_predictions)
             mae = recsys1.calc_mae(recsys1.val_rating_matrix, hybrid_predictions)
